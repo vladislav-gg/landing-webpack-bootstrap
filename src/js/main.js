69abdc1cd562caaaabf3c66bb7d4fb67bd8d380d
Vladislav <selies@mail.ru>
2022-08-04 20:09:02 +0100

    carousel adjustments made

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 // Import our custom CSS
 import "../sass/main.scss";
 
+// import mainImage from "../images/";
+
+// img.src = mainImage; // '/dist/151cfcfa1bd74779aadb.png'
+
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
